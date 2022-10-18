06197fd870d99c50d9d2322e00c588adc54c7af5
Devaki Poojitha <meherpoojitha1@gmail.com>
2022-10-18 11:07:37 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Compo1Component } from './compo1/compo1.component';
+import { Compo2Component } from './compo2/compo2.component';
+import { Compo3Component } from './compo3/compo3.component';
+import { Compo4Component } from './compo4/compo4.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Compo1Component,
+    Compo2Component,
+    Compo3Component,
+    Compo4Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/compo4/compo4.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-compo4',
+  templateUrl: './compo4.component.html',
+  styleUrls: ['./compo4.component.css']
+})
+export class Compo4Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
